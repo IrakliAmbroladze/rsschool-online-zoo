dabfec6b1ea9965d312b9586b10a84f7cf1c2d22
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-08 13:08:38 +0400

    refac(slider): made function more pure

--- online-zoo/script/main.js
+++ online-zoo/script/main.js
@@ -29,4 +29,16 @@ function setActiveNavLink() {
 
 loadComponent("#header", "/components/header.html");
 loadComponent("#footer", "/components/footer.html");
-slider();
+const left_arrow = document.getElementById("sldr_left_arr");
+const right_arrow = document.getElementById("sldr_right_arr");
+const slider_container = document.getElementById("slider");
+
+const container = document.getElementById("pets-container");
+const viewport = container.offsetWidth - 80; //deducting padding
+slider({
+  prev_btn: left_arrow,
+  next_btn: right_arrow,
+  slider: slider_container,
+  viewport,
+  cardSelector: ".animals-card",
+});
--- online-zoo/script/utils/slider.js
+++ online-zoo/script/utils/slider.js
@@ -1,36 +1,36 @@
-export const slider = () => {
-  const left_arrow = document.getElementById("sldr_left_arr");
-  const right_arrow = document.getElementById("sldr_right_arr");
-  const slider = document.getElementById("slider");
+export const slider = ({
+  prev_btn,
+  next_btn,
+  slider,
+  viewport,
+  cardSelector,
+}) => {
   const slider_width = slider.offsetWidth;
-  const container = document.getElementById("pets-container");
-  const viewport = container.offsetWidth - 80; //deducting padding
   const slider_overflow = slider_width - viewport;
   const slider_styles = window.getComputedStyle(slider);
-  const gap = Number(slider_styles.getPropertyValue("gap").slice(0, -2));
-  const card_width = document.querySelector(".animals-card").offsetWidth;
-
+  const gap = parseFloat(slider_styles.getPropertyValue("gap"));
+  const card_width = slider.querySelector(cardSelector).offsetWidth;
   const step_width = card_width + gap;
 
-  let accumulated_transformation = 0;
-  left_arrow.addEventListener("click", () => {
-    accumulated_transformation += step_width;
-    const remaining_overflow = slider_overflow + accumulated_transformation;
+  let offset = 0;
+  prev_btn.addEventListener("click", () => {
+    offset += step_width;
+    const remaining_overflow = slider_overflow + offset;
     if (remaining_overflow > slider_overflow) {
       slider.style.transform = `none`;
-      accumulated_transformation = 0;
+      offset = 0;
       return;
     }
-    slider.style.transform = `translate(${accumulated_transformation}px)`;
+    slider.style.transform = `translateX(${offset}px)`;
   });
-  right_arrow.addEventListener("click", () => {
-    accumulated_transformation -= step_width;
-    const remaining_overflow = slider_overflow + accumulated_transformation;
+  next_btn.addEventListener("click", () => {
+    offset -= step_width;
+    const remaining_overflow = slider_overflow + offset;
     if (remaining_overflow < 0) {
-      slider.style.transform = `translate(${-slider_overflow}px)`;
-      accumulated_transformation -= remaining_overflow;
+      slider.style.transform = `translateX(${-slider_overflow}px)`;
+      offset -= remaining_overflow;
       return;
     }
-    slider.style.transform = `translate(${accumulated_transformation}px)`;
+    slider.style.transform = `translateX(${offset}px)`;
   });
 };
