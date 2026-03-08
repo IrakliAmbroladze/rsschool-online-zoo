7d7eeef542030f08bbc0ec64a725b336204302e7
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-08 11:12:27 +0400

    fic(slider): replaced xCoordinate via hardcoded padding

--- online-zoo/script/utils/slider.js
+++ online-zoo/script/utils/slider.js
@@ -2,11 +2,9 @@ export const slider = () => {
   const left_arrow = document.getElementById("sldr_left_arr");
   const right_arrow = document.getElementById("sldr_right_arr");
   const slider = document.getElementById("slider");
-  const rect = slider.getBoundingClientRect();
   const slider_width = slider.offsetWidth;
-  const xCoordinate = rect.left;
   const container = document.getElementById("pets-container");
-  const viewport = container.offsetWidth - xCoordinate * 2; //deducting padding
+  const viewport = container.offsetWidth - 80; //deducting padding
   const slider_overflow = slider_width - viewport;
   const slider_styles = window.getComputedStyle(slider);
   const gap = Number(slider_styles.getPropertyValue("gap").slice(0, -2));
