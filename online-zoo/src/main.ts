3a66cb4510114407afec1e14859781e6060235c3
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-10 02:03:05 +0400

    refactor: change js files into ts

--- online-zoo/src/main.ts
+++ online-zoo/src/main.ts
@@ -1,11 +1,13 @@
-import { create_slider } from "./utils/slider/create_slider.js";
-async function loadComponent(selector, url) {
+import { create_slider } from "./utils/slider/create_slider";
+
+async function loadComponent(selector: string, url: string) {
   const el = document.querySelector(selector);
   const res = await fetch(url);
-  el.innerHTML = await res.text();
-
-  el.querySelector(".burger-menu")?.addEventListener("click", toggleMenu);
-  el.querySelector(".close-modal")?.addEventListener("click", toggleMenu);
+  if (el !== null) {
+    el.innerHTML = await res.text();
+    el.querySelector(".burger-menu")?.addEventListener("click", toggleMenu);
+    el.querySelector(".close-modal")?.addEventListener("click", toggleMenu);
+  }
   setActiveNavLink();
 }
 
@@ -14,11 +16,11 @@ function toggleMenu() {
   modal?.classList.toggle("show-burger-modal");
 }
 function setActiveNavLink() {
-  const normalize = (path) => path.replace(/\/$/, "") || "/";
+  const normalize = (path: string) => path.replace(/\/$/, "") || "/";
 
   const currentPath = normalize(window.location.pathname);
 
-  const links = document.querySelectorAll(".nav-link");
+  const links: NodeListOf<Element> = document.querySelectorAll(".nav-link");
 
   links.forEach((link) => {
     const linkPath = normalize(new URL(link.href).pathname);
