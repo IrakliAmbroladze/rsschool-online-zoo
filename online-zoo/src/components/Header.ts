1d4ec6a94262dbaeb8edc1626307793ccd326e0d
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-14 16:14:12 +0400

    feat(Header): add name and email in the modal for logged in user

--- online-zoo/src/components/Header.ts
+++ online-zoo/src/components/Header.ts
@@ -1,12 +1,20 @@
 import { createModal } from "../utils/createModal";
 
+type User = {
+  name: string;
+  email: string;
+};
+
 export const Header = (): void => {
   const usernameEl = document.getElementById("user-name");
   if (!usernameEl) return;
-  const username = "Irakli";
+  const user: User = {
+    name: "Irakli",
+    email: "ambroladzeirakli@gmail.com",
+  };
   const isLoggedIn = true;
   if (isLoggedIn) {
-    usernameEl.innerHTML = username;
+    usernameEl.innerHTML = user.name;
   }
   createModal({
     triggerSelector: ".user",
@@ -22,4 +30,14 @@ export const Header = (): void => {
 
   if (loggedIn) loggedIn.style.display = isLoggedIn ? "flex" : "none";
   if (notLoggedIn) notLoggedIn.style.display = isLoggedIn ? "none" : "flex";
+  const elUserLogged = document.querySelector(".logged-in");
+  if (elUserLogged) {
+    elUserLogged.innerHTML = `
+          <div>name: ${user.name}</div>
+          <div>${user.email}</div>
+          <button id="btn-sign-in" class="btn btn--orange">
+              Sign&nbspOut
+           </button>
+`;
+  }
 };
