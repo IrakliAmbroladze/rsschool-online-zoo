e554e62e52432e38fd7002128402ba27ba0ce80e
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-14 07:02:39 +0400

    refactor(main): extracted execution of slider creation

--- online-zoo/src/App.ts
+++ online-zoo/src/App.ts
@@ -1,6 +1,21 @@
 import { Header } from "./components/Header";
+import { create_slider } from "./utils/slider/create_slider";
 
 export default function App() {
+  const left_arrow = document.getElementById("sldr_left_arr");
+  const right_arrow = document.getElementById("sldr_right_arr");
+  const slider_container = document.getElementById("slider");
+
+  const container = document.getElementById("pets-container");
+  const viewport = container.offsetWidth - 80; //deducting padding
+  create_slider({
+    prev_btn: left_arrow,
+    next_btn: right_arrow,
+    slider: slider_container,
+    viewport,
+    cardSelector: ".animals-card",
+  });
+
   Header();
   console.log("This is an app component");
   return;
--- online-zoo/src/main.ts
+++ online-zoo/src/main.ts
@@ -1,20 +1,5 @@
 import App from "./App";
 import { loadComponent } from "./utils/loadComponent";
-import { create_slider } from "./utils/slider/create_slider";
-
-const left_arrow = document.getElementById("sldr_left_arr");
-const right_arrow = document.getElementById("sldr_right_arr");
-const slider_container = document.getElementById("slider");
-
-const container = document.getElementById("pets-container");
-const viewport = container.offsetWidth - 80; //deducting padding
-create_slider({
-  prev_btn: left_arrow,
-  next_btn: right_arrow,
-  slider: slider_container,
-  viewport,
-  cardSelector: ".animals-card",
-});
 
 async function main() {
   try {
