e3cf1a76cedd1e102c5db0fc1d89c5729d9176f5
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-17 22:42:09 +0400

    feat(Zoos): fetch cameras data in Zoos component

--- online-zoo/src/components/Zoos.ts
+++ online-zoo/src/components/Zoos.ts
@@ -1,3 +1,12 @@
+import { fetchCameras } from "../lib/fetchCameras";
+import type { Camera } from "../types/Camera";
+
 export default async function Zoos() {
+  try {
+    const cameras: Camera[] = await fetchCameras();
+    console.log(cameras);
+  } catch (error) {
+    console.log(error);
+  }
   console.log("start creating zoos page");
 }
