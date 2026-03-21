a1e7acc2ad7fdf12fbaef064cc79e1f5b6bcdc84
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-21 21:03:23 +0400

    chore(main): create main.tsx and App.tsx files to start react project

---
+++ online-zoo/src/App.tsx
@@ -0,0 +1,106 @@
+// import { Feedbacks } from "./components/Feedbacks";
+// import { Header } from "./components/Header";
+// import { MeetPets } from "./components/MeetPets";
+// import Zoos from "./components/Zoos";
+// import { REGISTRATION_INPUTS } from "./constants/registration";
+// import { SIGN_IN_INPUTS } from "./constants/signin";
+// import { createForm } from "./utils/createForm";
+// import { create_slider } from "./utils/slider/create_slider";
+export default function App() {
+  return (
+    <div>
+      <h1>start online-zoo</h1>
+    </div>
+  );
+}
+// export default async function App() {
+//   let user;
+//   const userFromStorage = localStorage.getItem("user");
+//   if (userFromStorage) {
+//     user = JSON.parse(userFromStorage);
+//   }
+//   Header({ user });
+//
+//   const page = document.body.className;
+//
+//   if (page === "page-landing") {
+//     try {
+//       await MeetPets();
+//     } catch (e) {
+//       console.error(e);
+//     }
+//     try {
+//       await Feedbacks();
+//     } catch (e) {
+//       console.error(e);
+//     }
+//     initPetsSlider();
+//     initFeedbackSlider();
+//   }
+//
+//   if (page === "page-zoos") {
+//     try {
+//       await Zoos();
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   }
+//
+//   if (page === "page-registration") {
+//     createForm({
+//       formNodeId: "form-registration",
+//       title: "Registration",
+//       inputs: REGISTRATION_INPUTS,
+//       endpointURL:
+//         "https://vsqsnqnxkh.execute-api.eu-central-1.amazonaws.com/prod/auth/register",
+//     });
+//   }
+//
+//   if (page === "page-sign-in") {
+//     createForm({
+//       formNodeId: "form-sign-in",
+//       title: "Sign In",
+//       inputs: SIGN_IN_INPUTS,
+//       endpointURL:
+//         "https://vsqsnqnxkh.execute-api.eu-central-1.amazonaws.com/prod/auth/login",
+//     });
+//   }
+// }
+//
+// function initPetsSlider() {
+//   const container = document.getElementById("pets-container");
+//   if (!container) return;
+//
+//   const left_arrow = document.getElementById("sldr_left_arr");
+//   const right_arrow = document.getElementById("sldr_right_arr");
+//   const slider_container = document.getElementById("slider");
+//   if (!left_arrow || !right_arrow || !slider_container) return;
+//
+//   const viewport = container.offsetWidth - 80;
+//   create_slider({
+//     prev_btn: left_arrow,
+//     next_btn: right_arrow,
+//     slider: slider_container,
+//     viewport,
+//     cardSelector: ".animals-card",
+//   });
+// }
+//
+// function initFeedbackSlider() {
+//   const feedback_container = document.getElementById(
+//     "feedback-content-container",
+//   );
+//   const feedback_slider = document.getElementById("feedback-container");
+//   const prev_arrow = document.getElementById("sldr_prev_arrow");
+//   const next_arrow = document.getElementById("sldr_next_arrow");
+//   if (!feedback_container || !feedback_slider || !prev_arrow || !next_arrow)
+//     return;
+//
+//   create_slider({
+//     prev_btn: prev_arrow,
+//     next_btn: next_arrow,
+//     slider: feedback_slider,
+//     viewport: feedback_container.offsetWidth,
+//     cardSelector: ".feedback-card",
+//   });
+// }
---
+++ online-zoo/src/main.tsx
@@ -0,0 +1,26 @@
+import { createRoot } from "react-dom/client";
+import "../style/scss/main.scss";
+import { StrictMode } from "react";
+import App from "./App";
+// import App from "./App";
+// import { loadComponent } from "./utils/loadComponent";
+
+// async function main() {
+//   try {
+//     await loadComponent("#header", "/components/header.html");
+//     await loadComponent("#footer", "/components/footer.html");
+//     await App();
+//   } catch (err) {
+//     if (err instanceof Error) {
+//       console.error("Caught an Error object:", err.message);
+//     } else {
+//       console.error("Caught an unknown error type:", err);
+//     }
+//   }
+// }
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
