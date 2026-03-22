4d02fbbaf143089cc4ec26b3c99408b669dfb779
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-23 01:04:05 +0400

    style: add wrapper to the project for occupying whole height while content is small

--- online-zoo/src/App.tsx
+++ online-zoo/src/App.tsx
@@ -5,6 +5,14 @@
 // import { REGISTRATION_INPUTS } from "./constants/registration";
 // import { SIGN_IN_INPUTS } from "./constants/signin";
 // import { createForm } from "./utils/createForm";
+// import { Header } from "./components/Header";
+// import { Footer } from "./components/Footer";
+// import { Route, Routes } from "react-router-dom";
+// import Home from "./components/Home";
+// import Map from "./components/Map";
+// import Zoos from "./components/Zoos";
+// import ContactUs from "./components/ContactUs";
+// import { create_slider } from "./utils/slider/create_slider";
 
 import { Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header";
@@ -14,10 +22,9 @@ import { Map } from "./components/Map";
 import { ContactUs } from "./components/ContactUs";
 import { Zoos } from "./components/Zoos";
 
-// import { create_slider } from "./utils/slider/create_slider";
 export default function App() {
   return (
-    <>
+    <div className="project-wrapper">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -26,17 +33,10 @@ export default function App() {
         <Route path="/contact-us" element={<ContactUs />} />
       </Routes>
       <Footer />
-    </>
+    </div>
   );
 }
 
-// import { Header } from "./components/Header";
-// import { Footer } from "./components/Footer";
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./components/Home";
-// import Map from "./components/Map";
-// import Zoos from "./components/Zoos";
-// import ContactUs from "./components/ContactUs";
 //
 // function App() {
 //   return (
