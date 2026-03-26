885ec4c7405c7fd926614198115f2c85948ba62c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-27 01:20:29 +0400

    feat(hooks): create usePets hook

--- online-zoo/src/main.tsx
+++ online-zoo/src/main.tsx
@@ -4,6 +4,7 @@ import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { FavouritesProvider } from "./context/FavoritesProvider";
+import { PetsProvider } from "./context/PetsProvider";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Root element not found");
@@ -11,9 +12,11 @@ if (!root) throw new Error("Root element not found");
 createRoot(root).render(
   <StrictMode>
     <FavouritesProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PetsProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PetsProvider>
     </FavouritesProvider>
   </StrictMode>,
 );
