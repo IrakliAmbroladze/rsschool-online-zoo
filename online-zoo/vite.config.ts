3c9786df4ba80622689102465c1b34241bb1bb70
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-03-26 12:04:01 +0400

    chore(vite.config): remove building options necessary for MPA. React does not need it

--- online-zoo/vite.config.ts
+++ online-zoo/vite.config.ts
@@ -1,19 +1,6 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        // map: resolve(__dirname, "map/index.html"),
-        zoos: resolve(__dirname, "zoos/index.html"),
-        contactUs: resolve(__dirname, "contact-us/index.html"),
-        registration: resolve(__dirname, "registration/index.html"),
-        signIn: resolve(__dirname, "sign-in/index.html"),
-      },
-    },
-  },
 });
