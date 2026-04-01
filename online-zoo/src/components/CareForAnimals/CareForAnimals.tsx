8a8c01108292e78146c16391c8ae3c1230116325
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-01 21:31:15 +0400

    chore: use panda cake image in care section

--- online-zoo/src/components/CareForAnimals/CareForAnimals.tsx
+++ online-zoo/src/components/CareForAnimals/CareForAnimals.tsx
@@ -6,8 +6,8 @@ export const CareForAnimals = () => {
     <section className={style.care}>
       <div className={style.container}>
         <PetCard
-          imgSource="/assets/images/panda.png"
-          imgAlt="panda"
+          imgSource="/assets/images/panda_cake.png"
+          imgAlt="panda-cake"
           description="Your $30 could give Lucas  a slice of panda cake, made with our secret recipe."
           btnText="feed"
         />
