8d9d1fb2308bf54b8cb1b7962aace530b1e2177b
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-01 23:01:14 +0400

    feat(CareForAnimals): generate cards based on navigation dot change

--- online-zoo/src/components/CareForAnimals/CareForAnimals.tsx
+++ online-zoo/src/components/CareForAnimals/CareForAnimals.tsx
@@ -3,22 +3,52 @@ import { DotNavigation } from "../DotNavigation/DotNavigation";
 import { PetCard } from "../PetCard/PetCard";
 import style from "./CareForAnimals.module.scss";
 
+const CARE_CARDS = [
+  {
+    image: "panda_care",
+    alt: "panda_care",
+    description:
+      "Your $30 could give Lucas  a slice of panda cake, made with our secret recipe.",
+    btnText: "feed",
+  },
+  {
+    image: "tiger_care",
+    alt: "tiger_care",
+    description:
+      " Your $150 will help to care for Senja,  a Sumatran tiger, for three weeks.",
+    btnText: "feed",
+  },
+  {
+    image: "lemur_care",
+    alt: "lemur_care",
+    description:
+      "With your support, we can give Andy his favorite fruits. Especially when it's not fruit season in its natural habitat.",
+    btnText: "feed",
+  },
+  {
+    image: "eagles_care",
+    alt: "eagles_care",
+    description:
+      "Sam & Lora have hatched and raised numerous young and will be happy with your help. ",
+    btnText: "feed",
+  },
+];
+
 export const CareForAnimals = () => {
   const [activeCardNumber, setActiveCardNumber] = useState<number>(0);
-  const getActiveDotNumber = (value: number) => {
-    setActiveCardNumber(value);
-  };
-  console.log("active dot is: ", activeCardNumber);
+
+  const activeCard = CARE_CARDS[activeCardNumber];
+
   return (
     <section className={style.care}>
       <div className={style.container}>
         <PetCard
-          imgSource="/assets/images/panda_cake.png"
-          imgAlt="panda-cake"
-          description="Your $30 could give Lucas  a slice of panda cake, made with our secret recipe."
-          btnText="feed"
+          imgSource={`/assets/images/${activeCard.image}.png`}
+          imgAlt={activeCard.alt}
+          description={activeCard.description}
+          btnText={activeCard.btnText}
         />
-        <DotNavigation getActiveDotNumber={getActiveDotNumber} />
+        <DotNavigation getActiveDotNumber={setActiveCardNumber} />
       </div>
     </section>
   );
