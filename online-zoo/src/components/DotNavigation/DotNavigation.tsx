d92a728a109b92856a9faee1f541ba55c8088729
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-01 22:11:15 +0400

    style(DotNavigation): add style for active dot

--- online-zoo/src/components/DotNavigation/DotNavigation.tsx
+++ online-zoo/src/components/DotNavigation/DotNavigation.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import style from "./DotNavigation.module.scss";
 export const DotNavigation = ({ dotNumber = 4 }: { dotNumber?: number }) => {
+  const [activeDot, setActiveDot] = useState<number>(0);
   const dotsArr = Array.from({ length: dotNumber });
   return (
     <div className={style.container}>
       {dotsArr.map((_, index) => {
-        return <div className={style.dot} key={index}></div>;
+        return (
+          <div className={`${style.dot} ${style.active}`} key={index}></div>
+        );
       })}
     </div>
   );
