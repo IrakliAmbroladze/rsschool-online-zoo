26eecb6f5e4b9004b4679a3f126e4e5ee6f63606
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-01 21:47:53 +0400

    feat(PetCard): add dots for navigation

--- online-zoo/src/components/DotNavigation/DotNavigation.tsx
+++ online-zoo/src/components/DotNavigation/DotNavigation.tsx
@@ -1,3 +1,11 @@
+import style from "./DotNavigation.module.scss";
 export const DotNavigation = () => {
-  return <div>dot navigation</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.dot}></div>
+      <div className={style.dot}></div>
+      <div className={style.dot}></div>
+      <div className={style.dot}></div>
+    </div>
+  );
 };
